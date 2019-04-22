e4d36f44c06db83f4537fcfd5edc93cb26dc87d9
mikeT13 <m.tsiroglou@hotmail.com>
2019-04-22 20:28:49 +0300

    fixed

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY ='AIzaSyD0z1gXU6-Cvm31JIFjfnu30ot14CvBGbs';
+const KEY ='';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
